53c4bb2c5202a8a2da4c4cb53bd557c88229e249
AndyLiDuke <144061629+AndyLiDuke@users.noreply.github.com>
2023-11-13 20:00:28 -0500

    Create Pull.js

---
+++ Pull.js
@@ -0,0 +1,12 @@
+function Pull(){
+ 
+   let bname = document.getElementById("brand").value;
+   let bdomain = document.getElementById("domain").value;
+   let pname = document.getElementById("product").value;
+  let disc = document.getElementById("discount").value;
+  let dd = 
+document.getElementById("details").value;
+  let text = 
+  "Create an Instagram Marketing post in 200 words for" + bname + "for their product" + pname + "which has a discount of" + dd +”. Make sure to include their domain name” + bdomain +” in the post
+  console.log(text);
+}
