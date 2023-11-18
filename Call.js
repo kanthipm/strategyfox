abe4d0c7e9d1ff334bac0330577d75fd10a073b4
Kanthi Makineedi <77213029+kanthipm@users.noreply.github.com>
2023-11-18 16:50:19 -0500

    Update Call.js

--- Call.js
+++ Call.js
@@ -3,9 +3,32 @@ function Call(){
   let bdomain = document.getElementById("domain").value;
   let pname = document.getElementById("product").value;
   let disc = document.getElementById("discount").value;
-  let dd = 
-document.getElementById("details").value;
-  let text = 
-  "Create an Instagram Marketing post in 200 words for" + bname + "for their product" + pname + "which has a discount of" + dd +”. Make sure to include their domain name” + bdomain +” in the post
+  let dd = document.getElementById("details").value;
+  if(disc == "Yes")
+  {
+  let prompt = "Create an Instagram Marketing post for " 
+    + bname 
+    + ". They are currenlty trying to promote thier product " 
+    + pname 
+    + ". To do so they are running a promotion. Their product "
+    + pname 
+    + " is currently being sold at a discount of " 
+    + dd 
+    + ". Make sure to include their domain name "
+    + bdomain 
+    + " in the post."
   console.log(text);
+  }
+  else
+  {
+      let prompt = "Create an Instagram Marketing post for " 
+    + bname 
+    + ". They are currenlty trying to promote thier product " 
+    + pname 
+    + ". Make sure to include their domain name "
+    + bdomain 
+    + " in the post."
+  }
+
+  fetch(https://diigtest.openai.azure.com/).then(response => {return response.text()}).then(reply => {document.getElementById('result').src = reply})
 }
