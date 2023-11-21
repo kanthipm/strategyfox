b36e6f1e94aab249f34eb63eb8667111690ff407
AndyLiDuke <144061629+AndyLiDuke@users.noreply.github.com>
2023-11-21 11:47:29 -0500

    Update Call.js

--- Call.js
+++ Call.js
@@ -1,6 +1,6 @@
 function Call(){
   let bname = document.getElementById("brand").value;
-  let bdomain = document.getElementById("domain").value;
+  let industry = document.getElementById("industry").value;
   let pname = document.getElementById("product").value;
   let disc = document.getElementById("discount").value;
   let dd = document.getElementById("details").value;
@@ -15,8 +15,8 @@ function Call(){
     + pname 
     + " is currently being sold at a discount of " 
     + dd 
-    + ". Make sure to include their domain name "
-    + bdomain 
+    + ". Make sure to include their industry "
+    + industry 
     + " in the post."
   }
   else
@@ -25,8 +25,8 @@ function Call(){
     + bname 
     + ". They are currenlty trying to promote thier product " 
     + pname 
-    + ". Make sure to include their domain name "
-    + bdomain 
+    + ". Make sure to include their industry "
+    + industry 
     + " in the post."
   }
 
