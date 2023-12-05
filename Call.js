a645cb1c7d5a5314b934824c59c66cc7138b625e
Kanthi Makineedi <77213029+kanthipm@users.noreply.github.com>
2023-12-04 22:24:40 -0500

    Update Call.js

--- Call.js
+++ Call.js
@@ -17,7 +17,7 @@ function Call(){
     + dd 
     + ". Make sure to include their industry "
     + industry 
-    + " in the post. Only include caption. Do not include instructions or extra information in the output."
+    + " in the post. Only include caption. Do not include instructions in the output."
   }
   else
   {
@@ -27,7 +27,7 @@ function Call(){
     + pname 
     + ". Make sure to include their industry "
     + industry 
-    + " in the post. Only include caption. Do not include instructions or extra information in the output."
+    + " in the post. Only include caption. Do not include instructions in the output."
   }
 
 
