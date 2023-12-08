c0a9a449e4c9fd0b98ac90771055f0d99c930391
Kanthi Makineedi <77213029+kanthipm@users.noreply.github.com>
2023-12-07 22:08:33 -0500

    Update Call.js

--- Call.js
+++ Call.js
@@ -43,7 +43,7 @@ function Call(){
           'api-key': '6280d74bdb2d4afd8ee1c4442e3d1c90',
       },
       body: data,
-  }).then(response => {return response.json()}).then(res=>{return res.choices[0].text}).then(reply => {document.getElementById('result').innerText = reply});
+  }).then(response => {return response.json()}).then(reply => {document.getElementById('result').innerText = reply});
   
 
 }
